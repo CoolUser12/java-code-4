af64ac7df30c38ea9c837d84821f698af9ea958a
CoolUser12 <hajiyousaf034@gmail.com>
2024-06-08 15:49:02 +0500

    code for weekday weekend

---
+++ program.js
@@ -0,0 +1,7 @@
+// checking for weekend or weekday
+let weekend = prompt("enter your day to check ")
+if(weekend==="saturday" || weekend==="sunday"){
+    alert("its an weekend ");
+}else{
+    alert("its a week day");
+}
